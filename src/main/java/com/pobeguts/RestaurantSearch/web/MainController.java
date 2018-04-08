a01620eea39170ee692743056022909e0d1acb65
ivanpobeguts <Ivan.Pobeguc@gmail.com>
2018-04-09 01:05:31 +0300

    fix tomcat !!!

--- src/main/java/com/pobeguts/RestaurantSearch/web/MainController.java
+++ src/main/java/com/pobeguts/RestaurantSearch/web/MainController.java
@@ -4,11 +4,12 @@ package com.pobeguts.RestaurantSearch.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
 
-    @RequestMapping("/users")
+    @RequestMapping(value = "/users", method = RequestMethod.GET)
     public String printHello(ModelMap model) {
         model.addAttribute("message", "Hello Spring MVC Framework!");
         return "users";
