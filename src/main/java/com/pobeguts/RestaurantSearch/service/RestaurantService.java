4b23c625140a8f05527f37264aa75950ab80d88b
Pobeguts Ivan <Ivan.Pobeguts@aton.ru>
2018-04-10 18:41:41 +0300

    POST request not working

--- src/main/java/com/pobeguts/RestaurantSearch/service/RestaurantService.java
+++ src/main/java/com/pobeguts/RestaurantSearch/service/RestaurantService.java
@@ -6,7 +6,9 @@ import com.pobeguts.RestaurantSearch.model.User;
 import com.pobeguts.RestaurantSearch.repository.RestaurantRepository;
 import com.pobeguts.RestaurantSearch.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class RestaurantService {
 
     private final UserRepository userRepository;
@@ -24,5 +26,9 @@ public class RestaurantService {
             return restaurantRepository.save(restaurant);
         }
         return null;
+    }
+
+    public Restaurant get (int id) {
+        return restaurantRepository.findById(id);
     }
 }
--- src/main/java/com/pobeguts/RestaurantSearch/web/RestaurantRestController.java
+++ src/main/java/com/pobeguts/RestaurantSearch/web/RestaurantRestController.java
@@ -1,19 +1,33 @@
 package com.pobeguts.RestaurantSearch.web;
 
+import com.pobeguts.RestaurantSearch.model.Restaurant;
 import com.pobeguts.RestaurantSearch.service.RestaurantService;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
+@RestController
 public class RestaurantRestController {
     private final RestaurantService service;
+    private static final Logger log = getLogger(RestaurantRestController.class);
 
     @Autowired
     public RestaurantRestController(RestaurantService service) {
         this.service = service;
     }
 
-//    @RequestMapping("/vote")
-//    public Restaurant addRestaurant(@RequestParam(value = "id") int id, @RequestParam(value = "restId") int restId) {
-//        return service.add()
-//    }
+    @RequestMapping(value = "/add_restaurant", method = RequestMethod.POST, consumes = "application/json")
+//    @ResponseStatus(HttpStatus.CREATED)
+    public void addRestaurant(@RequestBody String restaurant) {
+        log.info("@@@@@@@@@@@@@@@@@@@@@ Restaurant saved: ", restaurant);
+//        return service.add(restaurant, 100002);
+    }
+
+    @RequestMapping(value = "/restaurant", method = RequestMethod.GET)
+    public Restaurant getRestaurant(@RequestParam(value = "id") int id) {
+        return service.get(id);
+    }
 }
 
